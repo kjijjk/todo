3013fb22726e46a775ca1baaf91af91d5c21bf4e
user <user@DESKTOP-OA6IAJ4>
2024-08-23 16:22:39 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "종료";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Todo 추가";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "저장";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(328, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 398);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "일기";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(233, 358);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Location = new System.Drawing.Point(600, 18);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(188, 398);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Todo";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "시험지 복습",
+            "발표자료 준비",
+            "밀린 과제 정리"});
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 27);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(176, 354);
+            this.checkedListBox1.TabIndex = 9;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "일기 & Todo - 심경서";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string DataFolderPath = @"C:\data\log\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeApplication();
+        }
+
+        private void InitializeApplication()
+        {
+            // 폴더가 존재하지 않으면 생성
+            if (!Directory.Exists(DataFolderPath))
+            {
+                Directory.CreateDirectory(DataFolderPath);
+            }
+
+            // 현재 날짜로 달력 초기화
+            monthCalendar1.SelectionStart = DateTime.Today;
+            monthCalendar1.SelectionEnd = DateTime.Today;
+
+            LoadDiaryAndTodo();
+        }
+
+        private void LoadDiaryAndTodo()
+        {
+            // 선택된 날짜 가져오기
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+            string diaryFilePath = GetDiaryFilePath(selectedDate);
+            string todoFilePath = GetTodoFilePath(selectedDate);
+
+            // 일기 내용 로드
+            if (File.Exists(diaryFilePath))
+            {
+                textBox1.Text = File.ReadAllText(diaryFilePath);
+            }
+            else
+            {
+                textBox1.Text = string.Empty;
+            }
+
+            // TODO 목록 로드
+            checkedListBox1.Items.Clear();
+            if (File.Exists(todoFilePath))
+            {
+                string[] todoLines = File.ReadAllLines(todoFilePath);
+                foreach (var line in todoLines)
+                {
+                    var parts = line.Split('|');
+                    if (parts.Length == 2)
+                    {
+                        string todo = parts[0];
+                        bool isChecked = bool.Parse(parts[1]);
+                        checkedListBox1.Items.Add(todo, isChecked);
+                    }
+                }
+            }
+        }
+
+        private string GetDiaryFilePath(DateTime date)
+        {
+            return Path.Combine(DataFolderPath, $"log-{date:yyyyMMdd}.dat");
+        }
+
+        private string GetTodoFilePath(DateTime date)
+        {
+            return Path.Combine(DataFolderPath, $"todo-{date:yyyyMMdd}.dat");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 저장 버튼 클릭 시, 일기 및 TODO 목록 저장
+            SaveDiaryAndTodo();
+        }
+
+        private void SaveDiaryAndTodo()
+        {
+            // 선택된 날짜 가져오기
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+
+            // 일기 저장
+            string diaryFilePath = GetDiaryFilePath(selectedDate);
+            File.WriteAllText(diaryFilePath, textBox1.Text);
+
+            // TODO 목록 저장
+            string todoFilePath = GetTodoFilePath(selectedDate);
+
+            // TODO 목록을 텍스트와 체크 상태를 포함하여 저장
+            List<string> todoLines = new List<string>();
+            foreach (var item in checkedListBox1.Items)
+            {
+                bool isChecked = checkedListBox1.GetItemChecked(checkedListBox1.Items.IndexOf(item));
+                todoLines.Add($"{item.ToString()}|{isChecked}");
+            }
+            File.WriteAllLines(todoFilePath, todoLines);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // TODO 추가 버튼 클릭 시, 입력 상자를 통해 내용 입력
+            string todo = Prompt.ShowDialog("Enter TODO item", "Add TODO");
+            if (!string.IsNullOrWhiteSpace(todo))
+            {
+                checkedListBox1.Items.Add(todo, false);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // TODO 체크 상태 저장 버튼 클릭 시, 체크 상태 저장
+            SaveDiaryAndTodo();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            LoadDiaryAndTodo();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+// Form1 클래스가 정의된 파일 내에 Prompt 클래스를 추가합니다.
+public static class Prompt
+{
+    public static string ShowDialog(string text, string caption)
+    {
+        Form prompt = new Form()
+        {
+            Width = 500,
+            Height = 150,
+            FormBorderStyle = FormBorderStyle.FixedDialog,
+            Text = caption,
+            StartPosition = FormStartPosition.CenterScreen
+        };
+        Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+        TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+        Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+        confirmation.Click += (sender, e) => { prompt.Close(); };
+        prompt.Controls.Add(textBox);
+        prompt.Controls.Add(confirmation);
+        prompt.Controls.Add(textLabel);
+        prompt.AcceptButton = confirmation;
+
+        return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+    }
+}
